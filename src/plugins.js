121809f40cc84a56f54b617c5614e03cf2825d7f
lbzg <lbzg.dev@gmail.com>
2020-02-26 14:01:55 +0000

    removed unnecessary packages: vue-js-modal, ansi-html, jquery, @fortawesome, vue2-transitions, vue-google-autocomplete, autocomplete

--- src/plugins.js
+++ src/plugins.js
@@ -1,20 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { extend as ValidationExtend, ValidationProvider } from 'vee-validate'
 import { required as requiredValidation } from 'vee-validate/dist/rules'
 import Vue from 'vue'
 import VueFlatPickr from 'vue-flatpickr-component'
-import VueJsModal from 'vue-js-modal'
 import * as VueGoogleMaps from 'vue2-google-maps'
 
 import 'flatpickr/dist/flatpickr.css'
-import '@/assets/js/jquery.min.js'
-import '@/assets/js/bootstrap.min.js'
+// import '@/assets/js/jquery.min.js'
+// import '@/assets/js/bootstrap.min.js'
 // import '@/assets/css/bootstrap.min.css'
 import '@/assets/css/main.css'
 import '@/assets/css/autocomplete.css'
 import '@/assets/css/pacifico.css'
 
-Vue.use(VueJsModal)
 Vue.use(VueFlatPickr)
 Vue.use(VueGoogleMaps, {
   load: {
@@ -27,7 +24,5 @@ ValidationExtend('required', {
   ...requiredValidation,
   message: 'Please fill in this field.',
 })
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 export const validationProvider = ValidationProvider
